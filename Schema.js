9cf582dc5a67a69417cb822942adab621803fe4a
mentor-guvi-react <134543257+mentor-guvi-react@users.noreply.github.com>
2024-11-30 12:58:17 +0530

    Add files via upload

--- Schema.js
+++ Schema.js
@@ -10,7 +10,18 @@ const RegistrationSchema = new Schema({
     phonenumber: {type:String },
 });
 
+const BookingSchema = new Schema({
+    hotelId: {type:String, required:true},
+    selectedDate: {type:String, required:true},
+    selectedSeats: {type:String, required:true},
+    selectedTime: {type:String, required:true},
+    username: {type:String, required:true},
+    isCancelled: {type:Boolean, default:false},
+})
+
+
 const RegistrationModel = mongoose.model('Registration', RegistrationSchema);
+const BookingModel = mongoose.model('Booking', BookingSchema);
 
 
-module.exports = {RegistrationModel};
+module.exports = {RegistrationModel,BookingModel};
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { connectDb } = require("./db");
-const { RegistrationModel } = require("./Schema");
+const { RegistrationModel,BookingModel } = require("./Schema");
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -48,9 +48,65 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/create-booking", () => {
-  res.send("Booking created");
+app.post("/create-booking", async (req,res) => {
+  const {hotelId,selectedDate, selectedSeats , selectedTime,username  } =   req.body;
+  try {
+  const dbResponse =   await BookingModel.create({
+      hotelId,
+      selectedDate,
+      selectedSeats,
+      selectedTime,
+      username,
+      isCancelled: false
+    })
+    console.log(dbResponse,'dbResponse');
+    res.send("Booking created");
+  } catch (error) {
+    res.send('Booking failed');
+  }
 });
+
+
+app.get('/bookingDetails/:username' ,  async(req,res) => {
+  const {username} =  req.params
+  try{
+    const response =   await BookingModel.find({username});
+    res.send(response);
+  }
+  catch(error){
+    res.send('cant fetch booking details for '+ username)
+  }
+})
+
+
+app.get('/getBookingSlots/:hotelId/:selectedDate', async(req,res) => {
+  const {hotelId,selectedDate} =  req.params
+  try{
+     const dbresponse  =  await BookingModel.find({hotelId,selectedDate});
+     res.send(dbresponse)
+  } 
+  catch(error){
+    res.send('cant fetch booking slots for '+ hotelId)
+  }
+})
+
+
+app.put('/cancelBooking/:bookingId', async(req,res) => {
+  const {bookingId} = req.params;
+
+  console.log(bookingId,'bookingId');
+
+  const filter  = {_id : bookingId};
+  const update  = {isCancelled : true};
+
+  try {
+   const dbresponse =  await BookingModel.findOneAndUpdate( filter , update );
+    res.send('Booking cancelled');
+    
+  } catch (error) {
+    
+  }
+})
 
 app.listen(4000, () => {
   console.log("server is running on port 4000");
